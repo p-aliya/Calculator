2f4421cf50385b07166b9782e3ce055e7d99ac07
Puteri Aliya Seet <binnurfirdau@wisc.edu>
2024-09-19 22:07:08 -0500

    finished milestone 2

--- app/src/main/java/com/cs407/calculator/MainActivity.kt
+++ app/src/main/java/com/cs407/calculator/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cs407.calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,73 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val firstInput = findViewById<EditText>(R.id.firstNumber)
+        val secondInput = findViewById<EditText>(R.id.secondNumber)
+        val addButton = findViewById<Button>(R.id.addButton)
+        val minusButton = findViewById<Button>(R.id.minusButton)
+        val multiplyButton = findViewById<Button>(R.id.multiplyButton)
+        val divideButton = findViewById<Button>(R.id.divideButton)
+
+        addButton.setOnClickListener {
+            val firstNum = try {firstInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+            val secondNum = try {secondInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+
+            val result = firstNum + secondNum
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("RESULT", result.toString())
+
+            startActivity(intent)
+        }
+
+        minusButton.setOnClickListener {
+            val firstNum = try {firstInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+            val secondNum = try {secondInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+
+            val result = firstNum - secondNum
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("RESULT", result.toString())
+
+            startActivity(intent)
+        }
+
+        multiplyButton.setOnClickListener {
+            val firstNum = try {firstInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+            val secondNum = try {secondInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+
+            val result = firstNum * secondNum
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("RESULT", result.toString())
+
+            startActivity(intent)
+        }
+
+        divideButton.setOnClickListener {
+            val firstNum = try {firstInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+            val secondNum = try {secondInput.text.toString().toInt()}
+            catch (e: NumberFormatException) { 0 }
+
+            var result = ""
+            if (secondNum == 0){
+                result = "Error: Dividing By Zero"
+            } else {
+                result = (firstNum / secondNum).toString()
+            }
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("RESULT", result)
+
+            startActivity(intent)
         }
     }
 }
